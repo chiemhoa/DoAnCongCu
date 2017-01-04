c2c267126cf4de4afb56f874daa87a269736094c
maiquochau <maiquochausb@gmail.com>
2017-01-04 19:41:33 +0700

    webtruyen v1

---
+++ Webtruyen/Webtruyen/Areas/Admin/Content/ckeditor/config.js
@@ -0,0 +1,23 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+    config.syntaxhighlight_lang = 'csharp';
+    config.syntaxhighlight_hideControls = true;
+    config.language = 'vi';
+    config.filebrowserBrowseUrl = '/Areas/Admin/Content/ckfinder/ckfinder.html';
+    config.filebrowserImageBrowseUrl = '/Areas/Admin/Content/ckfinder/ckfinder.html?Type=Images';
+    config.filebrowserFlashBrowseUrl = '/Areas/Admin/Content/ckfinder/ckfinder.html?Type=Flash';
+    config.filebrowserUploadUrl = '/Areas/Admin/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = '/Areas/Admin/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl = '/Areas/Admin/Content/ckfinder/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+    //FCKConfig.ProcessHTMLEntities = false;
+    CKFinder.setupCKEditor(null, '/Areas/Admin/Content/ckfinder/');
+
+};
