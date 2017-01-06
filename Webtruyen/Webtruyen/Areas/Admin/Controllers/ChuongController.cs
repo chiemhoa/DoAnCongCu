5f3a3dcf8f8d6a05211646a8c100388ce019512a
Tran Chiem Hoa <chiemhoa99@gmail.com>
2017-01-06 18:57:40 +0700

    web truyen v5

---
+++ Webtruyen/Webtruyen/Areas/Admin/Controllers/ChuongController.cs
@@ -0,0 +1,125 @@
+﻿using Models;
+using Models.Dao;
+using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Webtruyen.Areas.Admin.Controllers;
+
+namespace Webtruyen.Areas.Admin.Controllers
+{
+    public class ChuongController : BaseController
+    {
+        // GET: Admin/Chuong
+        public ActionResult Index()
+        {
+             
+            var listC = new ChuongModel();
+            var model = listC.ListAll();
+            return View(model);
+
+        }
+
+        // GET: Admin/Chuong/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult ChuongtrongtruyenPartial(int id, int page = 1, int pageSize = 5)
+        {
+
+           var listC = new ChuongDao().ListAllpagelist(id,page,pageSize);
+            ViewBag.dsC = listC;
+            ViewBag.maT = new TruyenDao().ViewDetail(id);
+            return PartialView(listC);
+        }
+
+
+
+
+        // GET: Admin/Chuong/Create
+        [HttpGet]
+        public ActionResult Create(int id)
+        {
+            //ViewBag.maT = id;
+            return View();        
+        }
+
+        // POST: Admin/Chuong/Create
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create(CHUONG chuong,int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new ChuongDao();
+                //string maT = Request["matruyen"];
+                //int ma = Int32.Parse(maT);
+                long flat = dao.Insert(chuong,id);
+                if (flat > 0)
+                {
+                    return RedirectToAction("Details", "Truyen", new { id = id });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm không thành công");
+                }
+            }
+
+            return View();
+        }
+
+        // GET: Admin/Chuong/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var chuong = new ChuongDao().ViewDetail(id);
+            return View(chuong);
+        }
+
+        // POST: Admin/Chuong/Edit/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(CHUONG chuong)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new ChuongDao();
+                var result = dao.Update(chuong);
+                if (result)
+                {
+                    return RedirectToAction("Details", "Truyen",new { id = chuong.Matruyen });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật không thành công");
+                }
+            }
+            return View();
+        }
+
+        // GET: Admin/Chuong/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: Admin/Chuong/Delete/5
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            bool flat = new ChuongDao().Delete(id);
+            if(!flat)
+            {
+                ModelState.AddModelError("", "Xóa không thành công");
+            }
+            return View();
+        }
+    }
+}
---
+++ Webtruyen/Webtruyen/Areas/Admin/Controllers/TheloaitruyenController.cs
@@ -0,0 +1,94 @@
+﻿using Models.Dao;
+using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webtruyen.Areas.Admin.Controllers
+{
+    public class TheloaitruyenController : BaseController
+    {
+        // GET: Admin/Theloaitruyen
+        public ActionResult Index(int id)
+        {
+            //var model = new TruyentheloaiDao().ListTLT(id);
+            ViewBag.theloaitruyen = new TruyentheloaiDao().ListTLT(id);
+            ViewBag.mattruyen = id;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create(int id)
+        {
+            setViewBag();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(TRUYEN_TL entity,int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new TruyentheloaiDao();
+               
+                setViewBag();
+                long flat = dao.Insert(entity,id);
+                if (flat > 0)
+                {
+                    ModelState.AddModelError("", "Thêm thành công");
+                    return RedirectToAction("Index", "Theloaitruyen",new { id= id });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm không thành công");
+                }
+            }
+    
+            return View();
+        }
+        public void setViewBag(int? selectedId = null)
+        {
+            var dao = new TheLoaiDao();
+            ViewBag.Matheloai = new SelectList(dao.ListAll(), "Matheloai", "Tentheloai", selectedId);
+
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var ttl = new TruyentheloaiDao().ViewDetail(id);
+            setViewBag();
+            return View(ttl);
+        }
+        [HttpPost]
+        public ActionResult Edit(TRUYEN_TL entity, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new TruyentheloaiDao();
+                setViewBag();
+                bool flat = dao.Update(entity);
+                if (flat)
+                {
+                    ModelState.AddModelError("", "Cập nhật thành công");
+                    return RedirectToAction("Index", "Theloaitruyen", new { id = id });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật không thành công");
+                }
+            }
+
+            return View();
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var truyen = new TruyentheloaiDao().Delete(id);
+            return View();
+        }
+
+
+
+    }
+}
